d147174917e79fe876b1b78778dcd66058698868
rajendra1795 <rprasad@developerscode.com>
2022-11-14 12:38:45 +0530

    run server

---
+++ tables/src/RealEstate/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Real Estate Datebase</h2>
+      <h3>Real Estate Plots</h3>
+    </div>
+  )
+}
+
+export default Home
