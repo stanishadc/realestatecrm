b3f725b681c6459c88490c3299f2bcd87d6d2e4e
vijjidevelopers <vlakshmi@developerscode.com>
2022-11-29 18:52:08 +0530

    Task

--- server/index.js
+++ server/index.js
@@ -1,197 +1,74 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql2");
 
-
 const db = mysql.createPool({
-    host :"208.91.199.11", 
-    user :"ztravilr_reuser",
-    password : "!yzN4a97",
-    database :"ztravilr_realestatecrm"
+    host:"208.91.199.11",
+    user:"ztravilr_reuser",
+    password:"!yzN4a97",
+    database: "ztravilr_realestatecrm"
 });
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
 
 // Listen to API
 
-app.listen(5000, () => {
-    console.log(`Heloo World!...  Server is Running on Port: 5000`);
+app.listen(5001, () => {
+    console.log(`Heloo World!...  Server is Running on Port: 5001`);
 }); 
 
-// GET API (SELECT)
-
-app.get("/api/get/AccountType", (req,res) => {
-    const sqlGet = "SELECT * FROM AccountType;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/CommissionEntry", (req,res) => {
-    const sqlGet = "SELECT * FROM CommissionEntry;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/Commissions", (req,res) => {
-    const sqlGet = "SELECT * FROM Commissions;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-
-
-app.get("/api/get/Customer", (req,res) => {
-    const sqlGet = "SELECT * FROM Customer;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/CustomerDocuments", (req,res) => {
-    const sqlGet = "SELECT * FROM CustomerDocuments;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-
-app.get("/api/get/Designations/", (req,res) => {
-    const sqlGet = "SELECT * FROM Designations;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-
-app.get("/api/get/Dummy", (req,res) => {
-    const sqlGet = "SELECT * FROM Dummy;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/EmployeeDocuments", (req,res) => {
-    const sqlGet = "SELECT * FROM EmployeeDocuments;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/Employees", (req,res) => {
-    const sqlGet = "SELECT * FROM Employees;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/LandDocuments", (req,res) => {
-    const sqlGet = "SELECT * FROM LandDocuments;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/Lands", (req,res) => {
-    const sqlGet = "SELECT * FROM Lands;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/Login", (req,res) => {
-    const sqlGet = "SELECT * FROM Login;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/Passbook", (req,res) => {
-    const sqlGet = "SELECT * FROM Passbook;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/PassbookDocuments", (req,res) => {
-    const sqlGet = "SELECT * FROM PassbookDocuments;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/Payments", (req,res) => {
-    const sqlGet = "SELECT * FROM Payments;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-
-app.get("/api/get/PlotPayments", (req,res) => {
-    const sqlGet = "SELECT * FROM PlotPayments;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/Plots", (req,res) => {
-    const sqlGet = "SELECT * FROM Plots;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/Projects", (req,res) => {
-    const sqlGet = "SELECT * FROM Projects;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/Roles", (req,res) => {
-    const sqlGet = "SELECT * FROM Roles;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-app.get("/api/get/Users", (req,res) => {
-    const sqlGet = "SELECT * FROM Users;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
-
-// POST API (INSERT)
-
-app.post("/api/post", (req,res) => {
-    const { DummyId,Name,Plots,Status } = req.body;
-    const sqlInsert =
-         "INSERT INTO Dummy (DummyId, Name, Plots, Status) VALUES (?, ?, ?, ?)";
-    db.query(sqlInsert, [DummyId, Name, Plots, Status], (error, result) => {
-       if (error) {
-        console.log(error);
-       } 
-    });
-});
-
-// PUT API (UPDATE)
-
-app.put("/api/update/:id", (req,res) => {
-    const { id } = req.params;
-    const { DummyId, Name, Plots, Status } = req.body;
-    const sqlUpdate = "UPDATE employee SET DummyId = ?, Name = ?, Plots = ?, Status = ? WHERE id = ?";
-    db.query(sqlUpdate, [DummyId,Name, Plots, Status, id], (error, result) => {
-        if (error) {
-            console.log(error);
+app.post("/create", (req, res) => {
+    const accountName = req.body.accountName;
+  
+    db.query(
+      "INSERT INTO AccountType (accountName ) VALUES (?)",
+      [accountName],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Values Inserted");
         }
+      }
+    );
+  });
+  
+  app.get("/AccountType", (req, res) => {
+    db.query("SELECT * FROM AccountType", (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
         res.send(result);
+      }
     });
-});
+  });
+  
+  app.put("/update", (req, res) => {
+    const AccountTypeId = req.body.AccountTypeId;
+    const accountName = req.body.accountName;
+    db.query(
+      "UPDATE AccountType SET accountName = ? WHERE AccountTypeId = ?",
+      [accountName, AccountTypeId],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
+  
+  app.delete("/delete/:AccountTypeId", (req, res) => {
+    const AccountTypeId = req.params.AccountTypeId;
+    db.query("DELETE FROM AccountType WHERE AccountTypeId = ?", AccountTypeId, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+  
--- tables/src/App.js
+++ tables/src/App.js
@@ -4,39 +4,7 @@ import React from 'react';
 import './App.css';
 
 import Home from './Home';
-
-import AccountType from './AccountType/Home';
-import CommissionEntry from './CommissionEntry/Home';
-import Commissions from './Commissions/Home';
-import Customer from './Customer/Home';
-import CustomerDocuments from './CustomerDocuments/Home';
-import Designations from './Designations/Home';
-import Login from './Login/Home';
-import Passbook from './Passbook/Home';
-import PassbookDocuments from './PassbookDocuments/Home';
-import Payments from './Payments/Home';
-import PlotPayments from './PlotPayments/Home';
-import Plots from './Plots/Home';
-import Projects from './Projects/Home';
-import Roles from './Roles/Home';
-import Users from './Users/Home';
-
-/*import Dummy from './RealEstate/Dummy';
-import EmployeeDocuments from './RealEstate/EmployeeDocuments';
-import Employees from './Employees';
-import LandDocuments from './LandDocuments';
-import Lands from './Lands';
-import Login from './Login';
-import Passbook from './Passbook';
-import PassbookDocuments from './PassbookDocuments';
-import Payments from './Payments';
-import PlotPayments from './PlotPayments';
-import Plots from './Plots';
-import Projects from './Projects';
-import Roles from './Roles';
-import Users from './Users';*/
-
-
+import AccountType from './Masters/AccountType'
 function App () {
   return(
     <div>
@@ -44,21 +12,6 @@ function App () {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/accounttype' element={<AccountType/>}/>
-          <Route path='/commissionentry' element={<CommissionEntry/>}/>
-          <Route path='/commissions' element={<Commissions/>}/>
-          <Route path='/customer' element={<Customer/>}/>
-          <Route path='/customerdocuments' element={<CustomerDocuments/>}/>
-          <Route path='/designations' element={<Designations/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/passbook' element={<Passbook/>}/>
-          <Route path='/passbookdocuments' element={<PassbookDocuments/>}/>
-          <Route path='/payments' element={<Payments/>}/>
-          <Route path='/plotpayments' element={<PlotPayments/>}/>
-          <Route path='/plots' element={<Plots/>}/>
-          <Route path='/projects' element={<Projects/>}/>
-          <Route path='/roles' element={<Roles/>}/>
-          <Route path='/users' element={<Users/>}/>
-
         </Routes>
       </BrowserRouter>
     </div>
